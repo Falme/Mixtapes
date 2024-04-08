2f52cfee2646941b17087c3267fd27554ba1abd7
Falme Streamless <falme_streamless@hotmail.com>
2024-04-08 05:33:37 -0300

    Locked resolution to window not being resizable

--- Scripts/SplashScreen/Transition/scene_transition.gd
+++ Scripts/SplashScreen/Transition/scene_transition.gd
@@ -6,6 +6,6 @@ func _ready() -> void:
 	changeScene()
 	
 func changeScene():
-	# await get_tree().create_timer(3).timeout
-	#get_tree().change_scene_to_file("res://Scenes/collection.tscn")
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://Scenes/collection.tscn")
 	pass
