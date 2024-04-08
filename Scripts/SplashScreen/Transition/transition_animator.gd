aa4f164c14bd5139076885b475349851d6d96c7c
Falme Streamless <falme_streamless@hotmail.com>
2024-04-08 05:17:01 -0300

    Made some Fade In and Fade Out animation for SplashScreen

--- Scripts/SplashScreen/Transition/transition_animator.gd
+++ Scripts/SplashScreen/Transition/transition_animator.gd
@@ -6,22 +6,48 @@ var flag_colors_data_path = "res://Data/flagcolors.json"
 var flag_colors_data = {}
 var flag_colors_index = 0
 var flag_colors_size = 0
+var _flag_stripes: Array[Control] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_initialize_colors()
+	_initialize_flag_animation_instantiation()
+	_slide_in_flag_animation()
 	
+	await get_tree().create_timer(4).timeout
+	_slide_out_flag_animation()
+
+func _process(delta: float) -> void:
+	pass
+
+func _initialize_colors():	
+	# Load JSON File
 	flag_colors_data = load_json_file(flag_colors_data_path)
 	
+	# Randomize the Flag Colors
 	var rng = RandomNumberGenerator.new()
 	flag_colors_index = rng.randi_range(0, flag_colors_data["flags"].size()-1)
 	
+	#Save Flag Stripes Length for optimized call
 	flag_colors_size = flag_colors_data["flags"][flag_colors_index].size()
 	
+	# initialize Flag Stripes array
+	_flag_stripes.resize(80)
+
+func _initialize_flag_animation_instantiation():
 	flag_animation_instance = flag_animation_prefab.instantiate()
-	
+
+
+func _slide_in_flag_animation():
 	for n in 80:
 		await get_tree().create_timer(0.02).timeout
 		add_flag_strip(n)
+
+func _slide_out_flag_animation():
+	for n in 80:
+		await get_tree().create_timer(0.02).timeout
+		_flag_stripes[n].local_play("slide_down")
+	pass
 
 func add_flag_strip(index) -> void:
 	
@@ -34,6 +60,8 @@ func add_flag_strip(index) -> void:
 	_instance.set_color(Color(_r,_g,_b))
 	_instance.local_play("slide_up")
 	$".".add_child(_instance)
+	
+	_flag_stripes[index] = _instance
 
 func load_json_file(filePath : String):
 	if FileAccess.file_exists(filePath):
