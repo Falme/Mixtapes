ccc11e925515280fe4d97ba8e511ead637c77bc9
Falme Streamless <falme_streamless@hotmail.com>
2024-05-12 04:03:39 -0300

    Externated the game calls to a file called "mixtape_info.txt"

---
+++ Scripts/Collection/tape_configuration.gd
@@ -0,0 +1,42 @@
+extends TextureRect
+
+var this : TextureRect = self
+@export var gameNumber : String
+
+func _ready():
+	_load_info()
+
+func _load_info():
+	var filePath = OS.get_executable_path().get_base_dir()+"\\Games\\"+gameNumber+"\\mixtape_info.txt"
+	var file = FileAccess.open(filePath, FileAccess.READ)
+	var content = file.get_as_text()
+	
+	var infoArray = content.split("\r\n")
+	
+	var gameTitle = infoArray[0].split(":")[1]
+	var gameAuthor = infoArray[1].split(":")[1]
+	var gameBanner = infoArray[2].split(":")[1]
+	var gameExecutable = infoArray[3].split(":")[1]
+	
+	_load_title(gameTitle)
+	_load_author(gameAuthor)
+	_load_banner(gameBanner)
+	_load_executable(gameExecutable)
+
+func _load_title(_titleName):
+	$TapeLabel/TitleGame.text = _titleName
+	pass
+
+func _load_author(_authorName):
+	pass
+
+func _load_banner(_bannerName):
+	var bannerPath = OS.get_executable_path().get_base_dir()+"\\Games\\"+gameNumber+"\\"+_bannerName
+	var imageFile = Image.load_from_file(bannerPath)
+	
+	# Load image to Tape Background
+	this.texture = ImageTexture.create_from_image(imageFile)
+
+func _load_executable(_executablePath):
+	$ToGame.executableName = gameNumber+"\\"+_executablePath
+	pass
--- Scripts/ExternalGame/call_external_game.gd
+++ Scripts/ExternalGame/call_external_game.gd
@@ -3,5 +3,5 @@ extends Button
 @export var executableName : String
 
 func _on_pressed() -> void:
-	OS.execute(OS.get_executable_path().get_base_dir()+"\\\\Games\\"+executableName, [])
+	OS.shell_open(OS.get_executable_path().get_base_dir()+"/Games/"+executableName)
 	pass
