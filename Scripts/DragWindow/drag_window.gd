af83c4eb989007c4cf62751c11384e60970d3502
Falme Streamless <falme_streamless@hotmail.com>
2024-05-31 05:51:31 -0300

    Added Feature to be able to drag window around screen

---
+++ Scripts/DragWindow/drag_window.gd
@@ -0,0 +1,39 @@
+extends Node
+
+var isDragging : bool = false
+var cursorDelta : Vector2i
+var lastCursorPosition : Vector2i
+
+
+func _process(delta):
+	
+	cursorDelta = lastCursorPosition - _get_screen_mouse_position()
+	
+	
+	if isDragging:
+		var newWindowPosition : Vector2i = _get_screen_mouse_position() - Vector2i((get_window().size.x/2), 20)
+		
+		if(newWindowPosition.x < 0):
+			newWindowPosition.x = 0
+		if(newWindowPosition.y < 0):
+			newWindowPosition.y = 0
+		get_window().position = newWindowPosition
+	
+	
+	lastCursorPosition = _get_screen_mouse_position()
+
+func _on_button_down():
+	lastCursorPosition = _get_screen_mouse_position()
+	isDragging = true
+
+
+func _on_button_up():
+	isDragging = false
+
+func _get_screen_mouse_position():
+	
+	var viewportCursorPosition : Vector2i = get_viewport().get_mouse_position()
+	var windowPosition : Vector2i = get_window().position
+	var screenMousePosition = viewportCursorPosition + windowPosition
+	
+	return screenMousePosition
